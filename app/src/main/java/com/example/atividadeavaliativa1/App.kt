d9b6e0ae85a4c15aa13073a89c1b9deb9ce3dc05
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2025-05-17 16:48:08 -0300

    first commit

---
+++ app/src/main/java/com/example/atividadeavaliativa1/App.kt
@@ -0,0 +1,36 @@
+package com.example.atividadeavaliativa1
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.atividadeavaliativa1.ui.theme.AtividadeAvaliativa1Theme
+
+@Composable
+fun App () {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AtividadeAvaliativa1Theme {
+        Greeting("Android")
+    }
+}
