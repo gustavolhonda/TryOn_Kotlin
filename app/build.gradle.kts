983fdbf4ef62f558bf9dc26a55ea71f5ace971f6
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2025-05-24 12:23:04 -0300

    add gradle first configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.example.atividadeavaliativa1"
+    namespace = "com.example.login_kt2"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.atividadeavaliativa1"
+        applicationId = "com.example.login_kt2"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -49,7 +50,28 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Room dependencies
+    // it needs kapt plugin -> id("kotlin-kapt")
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("androidx.room:room-testing:$room_version")
+    androidTestImplementation("app.cash.turbine:turbine:0.9.0")
+
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
